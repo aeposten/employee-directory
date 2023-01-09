3b19317a0234ea44d5988cf3a8b5fd9319a8caa7
Amy <86963506+aeposten@users.noreply.github.com>
2023-01-09 10:31:45 -0800

    Can render ricks on page

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 // Fetches all characters from API
-function fetchCharacters() {
-    fetch('https://rickandmortyapi.com/api/character')
-        .then((response) => response.json())
-        .then((data) => console.log(data.results))
+const allRicksEl = document.getElementById("all-ricks");
+
+function fetchRicks() {
+  fetch("https://rickandmortyapi.com/api/character/?name=rick")
+    .then((response) => response.json())
+    .then((data) => renderRicks(data.results));
 }
 
-fetchCharacters();
+function renderRicks(rickArray) {
+  allRicksEl.innerHTML = rickArray
+    .map(
+      (rick) =>
+        `
+            <section class="rick">
+                <h2 class="rick-name">${rick.name}</h2>
+                <img class="rick-img" src="${rick.image}"/>
+                <div class="rick-status">${rick.status}</div>
+                <div class="rick-location">${rick.location.name}</div>
+                <div class="rick-description">${rick.name} is one of our star employees!</div>
+            </section>
+        `
+    )
+    .join("");
+}
+
+fetchRicks();
